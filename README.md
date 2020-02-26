0695b5083fb0c9770c2d71de14bf65d831830455
David <mydavidkoh@gmail.com>
2020-02-26 18:54:41 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # fortune500
 Index of Fortune 500 companies
+
+Getting raw data of fortune 500 companies for free is not easy.
+Doing a curl on Fortune 500 did not return me a list of html elements to look at the data.
+
+Essentially this became a web scraping project.
+If I can't look at Fortune 500, I looked at the components of the S&P 500 index.
+
+
+Yahoo Finance no longer supports api. Fortune 500 has no api. The web page is filled with junk and useless ads
+that makes trivial parsing more difficult.
+
+##Reference
+
+let baseUrl = "https://www.slickcharts.com/sp500";
+
+let baseUrl = "https://www.suredividend.com/sp-500-stocks/";
+
+
+Trading Economics
+https://tradingeconomics.com/
+
+### API
+
+https://github.com/cmusam/fortune500
+
+
+Yahoo Finance is outdated.
+
+Use Alpha Vantage (free. 5 api requests per min)
+https://www.alphavantage.co/documentation/
+
+World Trading data (free. 5 stocks per request)
+https://www.worldtradingdata.com/documentation#introduction
+
+IEX
+https://iexcloud.io/
+
+
+Trading Economics
+https://tradingeconomics.com/
--- src/index.js
+++ src/index.js
@@ -4,24 +4,72 @@ var fs = require('fs');
 var underscore = require('underscore');
 var chalk = require('chalk');
 var moment = require('moment');
+var jsdom = require('jsdom');
+var jquery = require('jquery');
 
-let baseUrl = "https://fortune.com/fortune500/";
+
+// Fortune website really sucks. can't web crawl data
+
+// If I can't look at Fortune 500 data, look at S&P 500 index data.
+// S&P 500 is US only. not global.
+
+let baseUrl = "https://www.slickcharts.com/sp500";
+
+// let baseUrl = "https://fortune.com/fortune500/2019/search/";
+// let baseUrl = "https://www.suredividend.com/sp-500-stocks/";
+
+// "https://finance.yahoo.com/quote/AAPL/financials?p=AAPL";
+
+
+
+// "https://fortune.com/fortune500/2019/walmart/";
 
 // need to do debug / verbose / trace mode
 
 
 /**
- * Fetch the basic info and names of fortune 500 companies
+ * Fetch the basic info and names of fortune 500 companies.
+ * Need to do web crawling... on HTML DOM elements
+
+ * Fetch HTML for page asynch
+ * Feed HTML to JSDOM and get DOM object in return
+ * feed DOM object into JQuery
+ * use JQuery to grab required pieces of data
+
  */
 function fetchCompanies() {
   let lst = [];
+
+  // console.log(baseUrl + "2019/search/");
+
   request(baseUrl, (error, response, body) => {
     if (response.statusCode != 200) {
       console.log("status code: " + response.statusCode);
       console.log("errored with the following message: " + error);
+      return;
     }
 
-    console.log("body: " + body);
+    // read ul class
+
+    // write to file
+    fs.writeFile('sp500.txt', body, (err) => {
+      if (err) throw err;
+    });
+
+    // jsdom.env({
+    //   html: body,
+    // }, (err, window) => {
+    //   var $ = window.jQuery;
+    //
+    // })
+
+
+    const {JSDOM} = jsdom;
+    const dom = new JSDOM(body);
+    const $ = jquery(dom.window);
+    var items = $(".thead");
+
+    console.log(items);
 
   });
 
